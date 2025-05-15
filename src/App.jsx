260a874335721acc552d586082540d909ee642f9
ogechi <princewillogechi7@gmail.com>
2025-05-15 18:38:55 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import React from "react";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+        
+  return (
+    <>
+      <div className='text-lg text-green-400'>
+        hello
+      </div>
+        
+    </>
+  )
+}
+
+export default App
